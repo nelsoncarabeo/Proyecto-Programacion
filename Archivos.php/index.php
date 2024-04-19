00d0034223e734a478be9100451a62450bf8850f
EduardoC22 <117883455+EduardoC22@users.noreply.github.com>
2024-04-18 20:38:06 -0600

    Primer commit en la rama main

---
+++ Archivos.php/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="css/login.css">
+
+
+</head>
+<body>
+    <form action="validar.php" method="POST">
+    <h1>Inicio de Sesión</h1>
+    <p>Usuario <input type="text" placeholder="ingrese su nombre"  name="usuario"></p>
+    <p>Contraseña <input type="password" placeholder="ingrese su contraseña"  name="contraseña"></p>
+    <input type="submit" value="Iniciar Sesión">
+
+    </form>
+</body>
+</html>
+
+
+
